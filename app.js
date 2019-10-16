26ff3d866885b8e35fc3a9ee6b9e0ec62cf1626a
Vishal Jaiswal <vjaiswal@techracers.io>
2019-10-16 17:53:26 +0530

    psql sequelize setup

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ import { urlencoded, json } from 'body-parser';
 import  morgan from 'morgan';
 
 import session from 'cookie-session';
+import db from './src/models';
 import routes from './src/routes/index';
 
 const app = express();
 app.use(session({ secret: 'sessionsecret' }));
 app.use(morgan('dev'));
-
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+  next();
+});
 app.use(urlencoded({ extended: false }));
 app.use(json());
 
@@ -19,5 +23,6 @@ app.use(express.static('../public'));
 
 app.use('/', routes);
 app.listen(3000, () => {
-  console.log('listening at 3000')
+  db.sequelize.sync();
+  // db.sequelize.sync({force : true });
 });
