067aa05eec8c854dc5d3e7e47831d281263223c1
Vishal Jaiswal <vjaiswal@techracers.io>
2019-10-16 18:38:30 +0530

    route for transaction

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import users from '../controllers/userController'
+import transaction from '../controllers/transactionController';
 
 const router = Router();
 
 // Routes
 router.post('/createUser', users.createUser);
+router.post('/transaction',transaction.validateAndPerformTransaction)
 
 export default router;
