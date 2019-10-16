65595e39d218fd3a6471eba85d6c15bc753ce03f
Vishal Jaiswal <vjaiswal@techracers.io>
2019-10-16 18:17:45 +0530

    transaction validation currency check

---
+++ src/controllers/transactionController.js
@@ -0,0 +1,24 @@
+import { default as models } from '../models/index';
+import of from 'await-of';
+
+const { user, transaction } = models;
+let insertDB
+const currencies = ['bitcoin', 'ethereum']
+let balance, wallet, max, currency
+export default class transactions {
+  static async validateAndPerformTransaction(req, res) {
+    let transactionObj = req.body
+    currency = transactionObj.currencyType
+    balance = currency + 'Balance'
+    wallet = currency + 'Wallet'
+    max = 'max' + currency.charAt(0).toUpperCase() + currency.slice(1)
+
+    transactionObj.createdAt = Date.now();
+    const validateRes = await transactions.validateTransaction(transactionObj)
+    res.send(validateRes)
+  }
+
+  static async validateTransaction(transactionObj) {
+    if (!currencies.includes(currency)) return { valid: false, message: 'Invalid Currency' }   
+  }
+}
