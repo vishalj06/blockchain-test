5f1aad00cb0089d5e1182c21f2c1a496742c6605
Vishal Jaiswal <vjaiswal@techracers.io>
2019-10-16 17:59:10 +0530

    user model

---
+++ src/models/user.js
@@ -0,0 +1,20 @@
+import bcrypt from 'bcrypt'
+export default (sequelize, DataTypes) => {
+
+    const User = sequelize.define('user', {
+        id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+        name: DataTypes.STRING,
+        description: DataTypes.TEXT,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        bitcoinWallet: { type: DataTypes.STRING, field: 'bitcoin_wallet', unique: true },
+        bitcoinBalance: { type: DataTypes.DECIMAL(10, 6), field: 'bitcoin_alance' },
+        maxBitcoin: { type: DataTypes.DECIMAL(10, 6), field: 'max_bitcoin' },
+        ethereumWallet: { type: DataTypes.STRING, field: 'ethereum_wallet', unique: true },
+        ethereumBalance: { type: DataTypes.DECIMAL(10, 6), field: 'ethereum_balance' },
+        maxEthereum: { type: DataTypes.DECIMAL(10, 6), field: 'max_ethereum' }
+    }, {});
+
+
+    return User;
+};
